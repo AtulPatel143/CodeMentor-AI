7f157fdd7d0a25c0326825573fb402fc17dadfc2
Addiction023 <atulpatel09876@gmail.com>
2026-07-21 15:26:41 +0530

    Restore Swagger route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -25,10 +25,8 @@ app.get("/", (_req, res) => {
 app.use("/api/tasks", taskRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
-app.get("/api-docs-test", (_req, res) => {
-  res.send("Swagger route is reachable");
-});
-//app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.get("/test", (_req, res) => {
   res.json({
     message: "Latest deployment is running!",
